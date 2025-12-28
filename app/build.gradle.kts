eba5f72d52877ced9bdf32758aa0b90f09f7a59e
202302010430齐洋 <202302010430@stu.zafu.edu.cn>
2025-12-28 13:44:34 +0800

    尝试多种Java版本配置以解决Gradle兼容性问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_25
+        targetCompatibility = JavaVersion.VERSION_25
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "25"
     }
 }
 
